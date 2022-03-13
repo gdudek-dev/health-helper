f2dcc34378ba8ccca757c7320b80f2585e79f7a7
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-03-13 12:23:53 +0100

    initial commit

---
+++ health-helper-api/src/main/java/com/gdudek/healthhelperapi/HealthHelperApiApplication.java
@@ -0,0 +1,13 @@
+package com.gdudek.healthhelperapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthHelperApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthHelperApiApplication.class, args);
+    }
+
+}
---
+++ health-helper-api/src/test/java/com/gdudek/healthhelperapi/HealthHelperApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdudek.healthhelperapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthHelperApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
