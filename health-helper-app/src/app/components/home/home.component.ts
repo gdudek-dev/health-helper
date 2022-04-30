cb762a409927a40db7970730d698702b2c2e6400
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-04-30 22:27:52 +0200

    implement home page layout

---
+++ health-helper-app/src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  userFirstName!: String;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
