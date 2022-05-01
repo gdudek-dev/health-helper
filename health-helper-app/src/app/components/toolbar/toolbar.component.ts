b3a484a6f418a94154caa2957b12aac2576f242b
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-05-01 17:45:10 +0200

    add logout to toolbar

--- health-helper-app/src/app/components/toolbar/toolbar.component.ts
+++ health-helper-app/src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageKey } from 'src/app/enums/local-storage.enum';
+import { AuthService } from 'src/app/services/authorization/auth.service';
+import { ToastService } from 'src/app/services/toast.service';
+import { TranslationService } from 'src/app/services/translation/translation-service';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private translationService: TranslationService,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    console.log("xd");
+    this.authService.logout().subscribe({
+      next: () => {
+        localStorage.removeItem(LocalStorageKey.SESSION_KEY);
+        this.router.navigate(['login']);
+      },
+      error: error => {
+        this.toastService.showNotification(
+          this.translationService.getTranslation("unable_to_connect")!,
+          this.translationService.getTranslation("cancel")!,
+          "error");
+      }
+    });
+  }
 }
