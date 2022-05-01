308de1d64a5b2f0e8ee82514090f44b2f2123857
Grzegorz Dudek <grzegorzdudekdev@gmail.com>
2022-05-01 12:27:45 +0200

    redesign home page

--- health-helper-app/src/app/components/home/home.component.ts
+++ health-helper-app/src/app/components/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  userFirstName!: String;
+  userFirstName: String = "Grzegorz"
 
   constructor() { }
 
